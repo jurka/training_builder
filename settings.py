b61bb592b61de33f8588ce63a6f82ebaa473297a
Iurii Ogiienko <iurii.ogiienko@gmail.com>
2012-01-20 01:30:03 +0200

    fixed typo

--- settings.py
+++ settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = '    urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(WORKDIR, 'templates')
